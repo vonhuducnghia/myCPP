d1cffc8af6e7eed00ea063250025699c4d0100fd
Nghia Vo Nhu Duc <vonhuducnghia@gmail.com>
2021-01-11 14:11:32 +0700

    Update Demo3.cpp

--- Demo3.cpp
+++ Demo3.cpp
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <iostream>
 using namespace std;
 
@@ -5,6 +6,9 @@ int main()
 {
 	string strName;
 	int nAge, nValue;
+	char strCopy1[] = "Hello";
+	char strCopy2[6];
+
 	cout << "Enter your age: ";
 	cin >> nAge;
 	cout << "Enter ASCII number: ";
@@ -17,4 +21,25 @@ int main()
 	cout << static_cast<char>(nValue) << endl;	//static_cast<type>(var) dùng để ép kiểu dữ liệu
 	// cout << strName.length() << endl;
 	// cout << strName.size() << endl;
+	strcpy(strCopy2, strCopy1);	//strcpy (str1, str2) use to copy string/array
+	cout << strName.length() << endl;
+	cout << sizeof(strName) << endl;
+	cout << strCopy2 << endl;
+	double a = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
+	double b = 1.0;
+	if (a == b)
+	{
+		cout << "a = b";
+	}
+	else if (a < b)
+	{
+		cout << "a < b";
+	}
+	else
+	{
+		cout << "a > b";
+	}
+	cout << "\n";
+	cout << a;
+	// open a file: fopen("FILE.txt", "r"); fclose()
 }
