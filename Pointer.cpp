9b946c9f8996d2eff1c8f0d4622b9adf81ae5c2c
Nghia Vo Nhu Duc <vonhuducnghia@gmail.com>
2021-02-16 16:28:25 +0700

    update

--- Pointer.cpp
+++ Pointer.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 
+// NOTATION:
+// A pointer is a variable that contains a memory location.
+// type *name;
 int main()
 {
+    /*
 	int x = 10;
 	cout << x << '\n';
 	cout << &x << '\n';
@@ -16,4 +20,16 @@ int main()
 
     cout << &i1 << endl;    //get address of i1
     cout << &i_ref << endl; //get address of i_ref
+    */
+    char lead;
+    char *sidekick;
+
+    lead = 'A';
+    sidekick = &lead;
+    cout << "About variable 'lead':\n";
+    cout << "Size\t" << sizeof(lead) << endl;
+    cout << "Contents\t" << lead << endl;
+    cout << "Location\t" << (void*)&lead << endl;
+    cout << "About variable 'sidekick': " << endl;
+    cout << "Contents\t" << (void*)sidekick << endl;
 }
