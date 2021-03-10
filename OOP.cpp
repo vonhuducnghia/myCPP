fd31d546d51eb8e08a52a5b8b4740df6915bf667
Nghia Vo Nhu Duc <vonhuducnghia@gmail.com>
2021-03-10 13:31:06 +0700

    Update OOP.cpp

--- OOP.cpp
+++ OOP.cpp
@@ -6,11 +6,17 @@ Write simple programs in C++ using the variables, operators, control structures,
 Write simple programs in C++ using objects and classes.
 Utilize the features of OPP.
 Develop the applicatuins using object oriented programming with C++
+
+YOU WILL NOT
+
+Be formally educated on C++
+Master C++ as a programming language
 */
 
 #include <iostream>
 using std::cout;
 using std::cin;
+using std::endl;
 
 int main()
 {
