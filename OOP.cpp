eafdf57502f34b6a10430aa0246b924a305d9b0e
Nghia Vo Nhu Duc <vonhuducnghia@gmail.com>
2021-03-10 13:16:50 +0700

    Create OOP.cpp

---
+++ OOP.cpp
@@ -0,0 +1,18 @@
+/*
+OUTCOME OF THE COURSE
+
+Describe the important concepts in OPP
+Write simple programs in C++ using the variables, operators, control structures, functions,...
+Write simple programs in C++ using objects and classes.
+Utilize the features of OPP.
+Develop the applicatuins using object oriented programming with C++
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main()
+{
+    
+}
