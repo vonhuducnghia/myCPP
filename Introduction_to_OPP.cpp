a96dfa7f526823b99c2b438c370b6ffbe20a5642
Nghia Vo Nhu Duc <vonhuducnghia@gmail.com>
2021-03-10 13:39:52 +0700

    Update Introduction_to_OPP.cpp

--- Introduction_to_OPP.cpp
+++ Introduction_to_OPP.cpp
@@ -15,10 +15,10 @@ Master C++ as a programming language
 YOU WILL
 
 Data abstraction
-Encapsulation
-Information hiding
-Polymorphism
-Inheritance
+Encapsulation: the proximity of data defs and operation defs.
+Information hiding: ability to selectively hide implementation details of a given ADT.
+Polymorphism: manipulate  different kinds of objects with only one operations.
+Inheritance: the ability of objects of one data type, to inherit operation and data from another data type.
 
 Using C++ to describe ideas
 */
