50aff96f6414a327f7e02495065882ab39173742
Nghia Vo Nhu Duc <vonhuducnghia@gmail.com>
2021-03-11 13:55:14 +0700

    Update Bit_Operators.cpp

--- Bit_Operators.cpp
+++ Bit_Operators.cpp
@@ -5,10 +5,31 @@ using std::endl;
 /* NOTATION
 <<, >>, >>>
 */
+int f(unsigned int a, unsigned int b)
+{
+    int c = 0;
+    while (b != 0)
+    {
+        if ((b && 1) != 0)
+        {
+            c = c + a;
+            a = a << 1;
+            b = b >> 1;
+            //left shift a by 1;
+            //right shift b by 1;
+        }
+    }
+    return c;
+}
+
 int main()
 {
-    int x, y;
-    x = 3;
-    y = x << 2;
-    cout << y;
+    unsigned int x, y;
+    unsigned int m, n;
+    x = 5;
+    y = 2;
+    n = 1;
+    cout << f(x, y);
+    m = n >> 1;
+    //cout << m;
 }
