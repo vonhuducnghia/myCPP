87223d5d74e821b034d57ef1b9cb2c65248344b6
Nghia Vo Nhu Duc <vonhuducnghia@gmail.com>
2021-03-15 15:35:30 +0700

    Update Demo1.cpp

--- Demo1.cpp
+++ Demo1.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main()
+{
+    string str;
+    int a;
+    string b;
+    cin >> str;
+    a = str.length();
+    b = str.substr(1, 4);
+    cout << a << endl;
+    cout << b << endl;
+}
