54d22103b738800ab7534adab708aecfa27cbd74
Nghia Vo Nhu Duc <vonhuducnghia@gmail.com>
2021-03-15 13:39:21 +0700

    Update Theo_CS.cpp

--- Theo_CS.cpp
+++ Theo_CS.cpp
@@ -5,10 +5,21 @@ using namespace std;
 int main()
 {
     string sTr;
-    int lenstr, indx;
+    int lenstr, sublen, indxsub, indx1, indx;
     cout << "Enter your string: ";
     cin >> sTr;
     cout << sTr << endl;
     lenstr = sTr.length();
-    cout << lenstr;
+    cout << lenstr << endl;
+    cout << "Enter index: ";
+    cin >> indx;
+    indx1 = indx - 1;
+    cout << "Enter substring length: ";
+    cin >> sublen;
+
+    indxsub = indx1 + sublen;
+    for (indx1; indx1 < indxsub; indx1++)
+    {
+        cout << sTr[indx1];
+    }
 }
