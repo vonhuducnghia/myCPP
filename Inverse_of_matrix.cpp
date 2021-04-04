6bf5b8c53ab4c31cb4af6fb3b1bf3ee117114d06
Nghia Vo Nhu Duc <vonhuducnghia@gmail.com>
2021-04-04 18:14:19 +0700

    Update Inverse_of_matrix.cpp

--- Inverse_of_matrix.cpp
+++ Inverse_of_matrix.cpp
@@ -497,7 +497,7 @@ std::ostream& operator <<(std::ostream &os, const CMatrix &m)
 #endif
 int main()
 {
-    CMatrix a("A", 5, 5);
+    CMatrix a("A", 6, 6);
     //std::cin >> a;
     a.FillSimulatedInput();
     CMatrix aadj = a.Inverse();
