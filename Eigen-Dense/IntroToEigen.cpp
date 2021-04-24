8472f03f47ee7fcc1af21163f1ad7fdd3f73576b
Nghia Vo Nhu Duc <vonhuducnghia@gmail.com>
2021-04-24 11:47:26 +0700

    Create IntroToEigen.cpp

---
+++ Eigen-Dense/IntroToEigen.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <Eigen/Dense>
+using namespace std;
+using namespace Eigen;
+
+int main()
+{
+	Eigen::MatrixXd d;
+	Matrix4d f;
+	f.size();
+	cout << f.size() << endl;
+	cin.get();
+}
