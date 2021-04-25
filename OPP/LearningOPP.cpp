8a1bf62f2ee46f6ffe4ba928471e46139b604436
Nghia Vo Nhu Duc <vonhuducnghia@gmail.com>
2021-04-25 12:23:31 +0700

    Update LearningOPP.cpp

--- OPP/LearningOPP.cpp
+++ OPP/LearningOPP.cpp
@@ -9,10 +9,9 @@ class YoutubeChannels
     string Name;
     string OwnerName;
     int SubcribersCount;
-
-    public:
     list<string> PublishedVideos; // Make a list of string (or any list of data type), ex: {"name1", "name2", "etc"}
 
+    public:
     YoutubeChannels(string name, string ownerName)
     {
         Name = name;
@@ -24,20 +23,35 @@ class YoutubeChannels
     {
         cout << "Name: " << Name << endl;
         cout << "Name: " << OwnerName << endl;
-        cout << "Playlist: ";
-        for (string Playlist : PublishedVideos) // Syntax to display an object list
+        cout << "SubcribersCount: " << SubcribersCount << endl;
+        for (string video : PublishedVideos)
         {
-            cout << Playlist << '\t';
+            cout << video << endl;
+        }
+    }
+    void publishedVideos(string title)
+    {
+        PublishedVideos.push_back(title);
+    }
+    void subcribersCount()
+    {
+        SubcribersCount++;
+    }
+    void unsubcriberCount()
+    {
+        if (SubcribersCount > 0 )
+        {
+            SubcribersCount--;
         }
     }
 };
 
 int main()
 {
-    YoutubeChannels myChannels("IreneAdler", "Nghia");
+    /*YoutubeChannels myChannels("IreneAdler", "Nghia");
     myChannels.PublishedVideos.push_back("LoL");
     myChannels.PublishedVideos.push_back("Overwatch");
-    myChannels.PublishedVideos.push_back("CSGO");
+    myChannels.PublishedVideos.push_back("CSGO");*/
 
     /*YoutubeChannels myChannels;
     Name = "IreneAdler";
@@ -51,6 +65,10 @@ int main()
     {
         cout << Playlist << '\t';
     }*/
+    YoutubeChannels myChannels("IreneAdler", "Nghia");
+    myChannels.publishedVideos("Overwatch");
+    myChannels.publishedVideos("CSGO");
+    myChannels.subcribersCount();
     myChannels.getinfo();
     //system("pause > 0");
 }
