08c69571aa4ea0565a545d82bd90216f37fa23aa
Nghia Vo Nhu Duc <vonhuducnghia@gmail.com>
2021-05-04 16:00:45 +0700

    update

--- Sorting/MergeSort.cpp
+++ Sorting/MergeSort.cpp
@@ -20,4 +20,59 @@ void MergeSort(int arr[], int l, int m, int r)
     {
         R[j] = arr[m + 1 + j];
     }
+
+    // Initial index of first subarray
+    int i = 0;
+ 
+    // Initial index of second subarray
+    int j = 0;
+ 
+    // Initial index of merged subarray
+    int k = l;
+ 
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    // Copy the remaining elements of
+    // L[], if there are any
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    // Copy the remaining elements of
+    // R[], if there are any
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// left index and right index of a sorted array
+void mergeSort(int arr[], int l, int r)
+{
+    if(l >= r)
+    {
+        return; //returns recursively
+    }
+    int m = l + (r - l) / 2;
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+    merge(arr, l, m, r);
 }
