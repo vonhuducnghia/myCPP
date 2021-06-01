63e2fec5ca7ba1d4f36758a925b72a639e76167c
Nghia Vo Nhu Duc <vonhuducnghia@gmail.com>
2021-06-01 17:42:28 +0700

    Update simpleDAC.cpp

--- English 2/simpleDAC.cpp
+++ English 2/simpleDAC.cpp
@@ -2,11 +2,15 @@
 #include <string>
 using namespace std;
 
+// Class for file 1
 class file1
 {
     public:
     string object;
+    construct()
+    {
     object = "file1";
+    }
 };
 
 class Alice : public file1
@@ -14,8 +18,93 @@ class Alice : public file1
     public:
     string subject;
     string action[4];
+    construct()
+    {
     subject = "alice";
     action = {"own", "read", "write", "execute"};
+    }
+};
+
+class Bob : public file1
+{
+    public:
+    string subject;
+    string action[1];
+    construct()
+    {
+    subject = "bob";
+    action = {"execute"};
+    }
+}
+
+class Macro : public file1
+{
+    public:
+    string subject;
+    string action[1];
+    construct()
+    {
+    subject = "macro";
+    action = {"read"};
+    }
+}
+// Class for file 2
+class file2
+{
+    public:
+    string object;
+    construct()
+    {
+    object = "file2";
+    }
+};
+
+class Alice : public file2
+{
+    public:
+    string subject;
+    string action[2];
+    construct()
+    {
+    subject = "alice";
+    action = {"read", "write"};
+    }
+};
+
+class Bob : public file2
+{
+    public:
+    string subject;
+    string action[1];
+    construct()
+    {
+    subject = "bob";
+    action = {"read"};
+    }
+};
+
+class Oscar : public file2
+{
+    public:
+    string subject;
+    string action[4];
+    construct()
+    {
+    subject = "oscar";
+    action = {"own", "read", "write", "execute"};
+    }
+};
+
+class Mary : public file2
+{
+    public:
+    string subject;
+    string action[1];
+    construct()
+    {
+    subject = "mary";
+    action = {"read"};
+    }
 };
 
 int main()
