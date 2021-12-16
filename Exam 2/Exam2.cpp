6c575dc976cec331cde1d4190a93becf82895b19
Nghia Vo Nhu Duc <nghiathechosenone@gmail.com>
2021-12-16 17:51:11 +0700

    Update Exam2.cpp

--- Exam 2/Exam2.cpp
+++ Exam 2/Exam2.cpp
@@ -1,18 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*double findShapeArea(double cord[][2], int n)
-{
-   double area = 0.0;
-   int j = n - 1;
-   for (int i = 0; i < n; i++) {
-      area += (float)(cord[j][0] + cord[i][0]) * (cord[j][1] - cord[i][1]);
-      j = i;
-   }
-
-   return abs(area / 2.0);
-}*/
-
 double findShapeperimeter(double cord[][2], int n) {
    
    double perimeter = 0.0;
@@ -24,19 +12,8 @@ double findShapeperimeter(double cord[][2], int n) {
    return perimeter;
 }
 
-/*int isEquableShape(double cord[][2], int n)
+int main()
 {
-   int area = findShapeArea(cord, n);
-   int peri = findShapeperimeter(cord, n);
-   cout<<"The area of the given shape is "<<area<<endl;
-   cout<<"The perimeter of the given shape is "<<peri<<endl;
-   if (area == peri)
-      return 1;
-   else
-      return 0;
-}*/
-
-int main() {
    
    int n = 4;
    double cord[n][2] = {{0, 0} , {2, 0}, {2, 2}, {0, 2}};
